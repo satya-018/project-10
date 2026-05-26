da063705bbdfdb324b6bb2632bace2eab814c6cc
Satya <teamdevil765@gmail.com>
2026-05-26 22:09:40 +0530

    Update q25.py

--- q25.py
+++ q25.py
@@ -25,8 +25,8 @@ if st.button("Check Countdown"):
     days_left = (next_birthday - today).days
 
     st.success(
-        f"🎉 birthday gya bhar me {title}! "
-        f" Toh tereko lagta hai mai jhut bol reha hu . Toh bs man le simple ."
+        f"🎉 congrulation {title}! "
+        f" your birthday is just {days_left} days away."
     )
 
     st.balloons()
